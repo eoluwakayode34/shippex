628185a9876b8370aec28c6061909f9956062a4a
emmy-gold <160324008+emmy-gold@users.noreply.github.com>
2024-03-18 08:24:17 +0100

    add fonts assets and react-native-paper

--- App.tsx
+++ App.tsx
@@ -9,13 +9,26 @@ import React, {useState} from 'react';
 
 import Splash from './src/components/layout/splash';
 import WelcomeScreen from './src/screens/auth/welcome';
+import {MD3LightTheme as DefaultTheme, PaperProvider} from 'react-native-paper';
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#2F50C1',
+  },
+};
 
 function App(): React.JSX.Element {
   const [closeSplash, setCloseSplash] = useState(true);
-  return closeSplash ? (
-    <Splash setCloseSplash={setCloseSplash} />
-  ) : (
-    <WelcomeScreen />
+  return (
+    <PaperProvider theme={theme}>
+      {closeSplash ? (
+        <Splash setCloseSplash={setCloseSplash} />
+      ) : (
+        <WelcomeScreen />
+      )}
+    </PaperProvider>
   );
 }
 
